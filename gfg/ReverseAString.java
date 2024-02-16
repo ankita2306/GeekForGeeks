ab3ec6e0206e4ce3dfc120c5a70dbae6db2dc72e
sandeep7762 <sandeep.sharma1819@gmail.com>
2024-02-16 18:06:44 +0530

    first code added

--- gfg/ReverseAString.java
+++ gfg/ReverseAString.java
@@ -4,11 +4,11 @@ public class ReverseAString {
     public static void main(String[] args) {
         String myString="Ankita";
         int length=myString.length();
-        String reverseString="";
+        StringBuilder reverseString= new StringBuilder();
         for(int i=length-1;i>=0;i--)
         {
             char ch=myString.charAt(i);
-            reverseString=reverseString+ch;
+            reverseString.append(ch);
         }
         System.out.println("My String --> "+myString);
         System.out.println("Reversed String --> "+reverseString);
